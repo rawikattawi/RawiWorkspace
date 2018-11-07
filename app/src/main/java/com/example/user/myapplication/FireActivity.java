4b8174bfdb71a411734684cde7fc5807870a9b05
rawi <rawikattawi29>
2018-11-07 14:16:39 +0200

    "database"

--- app/src/main/java/com/example/user/myapplication/FireActivity.java
+++ app/src/main/java/com/example/user/myapplication/FireActivity.java
@@ -2,8 +2,13 @@ package com.example.user.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,7 +24,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+@SuppressLint("Registered")
 public class FireActivity extends AppCompatActivity implements View.OnClickListener{
     EditText etEmail;
     EditText etPass;
@@ -30,17 +38,26 @@ public class FireActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         final FirebaseDatabase firebaseDatabase;
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference().child("Name");
+        final DatabaseReference myRef = database.getReference();
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                tvEmail.setText(value);
+                Map<String,String> map = (Map<String,String>) dataSnapshot.getValue();
+                Log.v ("E_VALUE", "DATA" + dataSnapshot.getValue());
+                String name = map.get("name");
+                String profession = map.get("profession");
+                tvEmail.setText(name);
+                tvProfession.setText(profession);
+
+//                String value = dataSnapshot.getValue(String.class);
+//                tvEmail.setText(value);
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
+
 
             }
         });
@@ -53,12 +70,48 @@ public class FireActivity extends AppCompatActivity implements View.OnClickListe
         tvEmail = findViewById(R.id.tvEmail);
         tvProfession = findViewById(R.id.tvProfession);
         btSave = findViewById(R.id.btSave);
-        btSave.setOnClickListener(this);
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              String email = etEmail.getText().toString();
+              myRef.child("Name").setValue(email);
+              myRef.child("profession").setValue("student");
+              myRef.child("Name").push().setValue(email);
+            }
+        });
 
          lvUsers = findViewById(R.id.lvUsers);
          users = new ArrayList<String>();
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
         lvUsers.setAdapter(adapter);
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String name = dataSnapshot.getValue(String.class);
+                users.add(name);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
